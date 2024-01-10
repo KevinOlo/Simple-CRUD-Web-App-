75aad0721f34a87bf503cae8a330a8db4662a5c1
kevo1121 <kevoofemi@gmail.com>
2024-01-09 21:19:45 -0500

    Working code changes for master

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)  # tells flask app all you need is in the current working dir
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"  # tells app where DB is located 3 / = relative path 4 is an absolute path
-app.config["SQLALCHEMY_BINDS"] = {"credentials": "sqlite:///credentials.db"}
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"  # tells app where DB is located 3 / = relative path 4 is an absolute path
 app.secret_key = 'raw_key'
 db = SQLAlchemy(app)
 app.app_context().push()  # ensures that you are within the application context when calling db.create_all()
 
 class login(db.Model):
-  __bind_key__ = "credentials"
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(30), unique=True, nullable=False)
   password = db.Column(db.String(20), nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
 
+  def __repr__(self):
+      return f'<login {self.username}>'
+
 class dbase(db.Model):  # Info in DB
   id = db.Column(db.Integer, primary_key=True)
   content = db.Column(db.String(200), nullable=False)
   date_created = db.Column(db.DateTime, default=datetime.utcnow)
-  '''user_id = db.Column(db.Integer, db.ForeignKey('login.id'), nullable=False)
-  user = db.relationship('login', backref=db.backref('tasks', lazy=True))'''
-  
+  user_id = db.Column(db.Integer, db.ForeignKey('login.id'), nullable=False)
+  user = db.relationship('login', backref=db.backref('tasks', lazy=True))
+
+  def __repr__(self):
+      return f'<dbase {self.user_id}>'  
 
 def __repr__(self):
   return ("<Task %r>" % self.id)  # returns task and its name when a new task is created, %r is a placeholder for self.id
@@ -31,13 +34,6 @@ def __repr__(self):
 @app.route("/", methods=["GET"])
 def homepage():
   return render_template('home.html')
-
-
-'''@app.route('/home/', methods=['GET', 'POST'])
-def index(): 
-  return render_template('index.html')'''
-
-
 
 @app.route("/login/", methods=["GET", "POST"])
 def login_web():
@@ -48,12 +44,14 @@ def login_web():
     user_check = login.query.filter_by(username=username).first()
 
     if user_check and user_check.password == password:
-      print (f"{username} has entered the Task Manager")
+      flash (f"{username} has entered the Task Manager, Welcome!", category='success')
       session['login'] = True
       session['username'] = username
+      session['user_id'] = user_check.id    #Store user_id in the session
       return redirect('/home/')
     else:
-      return "Invalid username or password"
+       flash("Invalid username or password", category='error')
+       return redirect('/login/')
 
   return render_template("login.html")
 
@@ -75,30 +73,26 @@ def register():
 
         # Check for empty values
     if not username or not password or not email:
-      return "Username and password and email cannot be empty"
+      flash("Username and password and email cannot be empty")
 
     existing_user = login.query.filter_by(username=username).first()
     existing_email = login.query.filter_by(email=email).first()
 
 
     if existing_user:
-      return "Sorry, this username has been taken"
+      flash("Sorry, this username has been taken")
     elif existing_email:
-      return "This email is already in use"
+      flash("This email is already in use")
     else:
       newuser = login(username=username, password=password, email=email)
       db.session.add(newuser)
       db.session.commit()
+      flash("Your acctount has been successfully created", category='success')
     
-      return redirect('/registration_success/')   
+      return redirect('/login/')   
 
   return render_template('register.html')
 
-
-
-@app.route('/registration_success/', methods=['POST'])
-def registration_success():
-  return redirect('/registration_success/')
 
 
 
@@ -108,15 +102,16 @@ def home():
   if session.get('login'):
     if request.method == 'POST':  # index.html form interaction
       task_content = request.form['content']
-      new = dbase(content=task_content)
+      new = dbase(content=task_content, user_id=session['user_id'])
 
       try:
         db.session.add(new)
         db.session.commit()
-        return redirect('/index/')  # redirect base to index.html
+        flash('Task added successfully', category='success')
+        return redirect('/home/')  # redirect base to index.html
 
       except:
-        return 'Error adding task'
+        flash('Error adding task', category='error')
 
     else:
       tasks = dbase.query.order_by(dbase.date_created).all()  # show all tasks sorted by creation date
@@ -124,7 +119,8 @@ def home():
       # return html template to web app
 
   else:
-    return 'Please login'
+    flash ('Please login')
+    return redirect('/login/')
     #return redirect('/login/')
 
 
